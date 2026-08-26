a6c860f1802714acadd66ec4c723c8229e5d7db9
ArunSonali2026 <apdelhi111@gmail.com>
2026-08-26 13:20:23 +0530

    Update cookie sameSite attribute to 'none'
    Changed cookie sameSite attribute from 'lax' to 'none'.

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "none",
     secure: process.env.NODE_ENV === "production",
     maxAge: 1000 * 60 * 60 * 8
   }
